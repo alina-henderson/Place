98d64b549d7fdb278b38320a23c3fd77d3dd9184
Alina Henderson <alina.henders@gmail.com>
2021-11-19 12:55:16 -0800

    feat: load popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,20 @@
+console.log("loaded")
+const editLink = document.querySelector(".profile__edit-button_type_open")
+const popup = document.querySelector(".popup")
+const popupCloseButton = popup.querySelector(".popup__close-icon")
+const addLink = document.querySelector(".profile__add-button_type-open")
+
+
+function open() {
+  popup.classList.add("popup_opened");
+}
+
+function close() {
+  popup.classList.remove("popup_opened");
+}
+
+editLink.addEventListener('click', open);
+popupCloseButton.addEventListener('click', close);
+addLink.addEventListener('click', open);
 
 
