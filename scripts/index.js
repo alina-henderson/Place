fd499e1ae6f9780632ac7f9d05f5fa2af6aabac6
Alina Henderson <alina.henders@gmail.com>
2021-11-20 00:22:27 -0800

    bufix: change like button

--- scripts/index.js
+++ scripts/index.js
@@ -5,17 +5,32 @@ const popupCloseButton = popup.querySelector(".popup__close-icon");
 const addButton = document.querySelector(".profile__add-button_type-open");
 const popupOverlay = popup.querySelector(".popup__overlay");
 
+const form = popup.querySelector(".form");
+const nameInput = popup.querySelector(".popup__name");
+const occupationInput = popup.querySelector(".popup__occupation");
+
+
 
 
 function open() {
   popup.classList.add("popup_opened");
+  nameInput.value = nameValue.textContent;
+  occupationInput.value = occupationValue.textContent;
 }
 
 function close() {
   popup.classList.remove("popup_opened");
 }
 
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  nameOutput.textContent = nameInput.value;
+  occupationOutput.textContent = occupationInput.value;
+
+
 editButton.addEventListener('click', open);
 popupCloseButton.addEventListener('click', close);
 addButton.addEventListener('click', open);
 popupOverlay.addEventListener('click', close);
+form.addEventListener('submit', formSubmitHandler);
+
