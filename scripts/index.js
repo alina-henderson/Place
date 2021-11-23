35b64ff9b9995b3e54ea0b1f241abd5fa4c2c653
Alina Henderson <alina.henders@gmail.com>
2021-11-22 23:59:35 -0800

    bufix: correct html JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,14 @@
-const editButton = document.querySelector(".profile__edit-button_type_open");
+const editButton = document.querySelector(".profile__edit-button");
 const popup = document.querySelector(".popup");
 const popupCloseButton = popup.querySelector(".popup__close-icon");
-const addButton = document.querySelector(".profile__add-button_type-open");
 const form = popup.querySelector(".form");
-const nameInput = popup.querySelector(".form__name");
-const occupationInput = popup.querySelector(".form__occupation");
+const nameInput = popup.querySelector(".form__input_value_name");
+const occupationInput = popup.querySelector(".form__input_value_occupation");
 const nameValue = document.querySelector(".profile__name");
 const occupationValue = document.querySelector(".profile__occupation");
 
 function open() {
+  
   popup.classList.add("popup_opened");
   nameInput.value = nameValue.textContent;
   occupationInput.value = occupationValue.textContent;
@@ -20,11 +20,11 @@ function close() {
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  nameOutput.textContent = nameInput.value;
-  occupationOutput.textContent = occupationInput.value;
+  nameValue.textContent = nameInput.value;
+  occupationValue.textContent = occupationInput.value;
+  close();
 }
 
 editButton.addEventListener('click', open);
 popupCloseButton.addEventListener('click', close);
-addButton.addEventListener('click', open);
 form.addEventListener('submit', formSubmitHandler);
