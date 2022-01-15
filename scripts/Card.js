b598b3d909e66e7473c360154cfe9784794776a3
alina-henderson <alina.henders@gmail.com>
2022-01-15 11:22:08 -0800

    feat: add classes

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,10 +1,28 @@
 class Card {
-  constructor(name, link, alt) {
-      this.name = name;
-      this.link = link;
-      this.alt = alt;
+  constructor(selector, name, link, alt) {
+    this._selector = selector;
+      this._name = name;
+      this._link = link;
+      this._alt = alt;
   }
 
+  //create a card
+  _getTemplate() {
+    return document.querySelector(this._selector)
+    .content
+    .querySelector('.element')
+    .cloneNode(true)
+  }
+
+  //get template , saved initial data and insert them in span
+  getView() {
+    this._element = this._getTemplate();
+    this._element.querySelector('.element__title');
+    
+
+
+    return this._getTemplate();
+  }
 }
 
 export default Card;
--- scripts/index.js
+++ scripts/index.js
@@ -176,3 +176,8 @@ const initialCards = [
 // addButton.addEventListener('click', openAddProfile);
 
 // render();
+
+
+//render the list of elements/get the the link to the section
+const elements = document.querySelector('.elements');
+
