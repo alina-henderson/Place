093c491d346282e5262a85835126de4b8eac1693
alina-henderson <alina.henders@gmail.com>
2022-02-05 20:03:48 -0800

    feat: add open to popup class

--- components/Popup.js
+++ components/Popup.js
@@ -3,13 +3,13 @@ export default class Popup {
   constructor(selector) {
     this._selector = document.querySelector(selector);
         this._overlay = this._selector.querySelector('.popup__overlay');
-        this._closeButton = this._selector.querySelector('.popup__close');
+        this._closeButton = this._selector.querySelector('.button-close');
         this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   open() {
-
-
+    this._selector.classList.add('popup_opened');
+    document.addEventListener('keydown', (evt) => this._handleEscClose(evt));
   }
 
   close() {
