f3c946fdf1a5cb8083a26a6a33265f4ae0fa86b7
alina-henderson <alina.henders@gmail.com>
2022-02-05 19:49:16 -0800

    feat: add renderItems to section

--- components/Section.js
+++ components/Section.js
@@ -1,20 +1,22 @@
 
 export default class Section {
-  constructor({items, renderer}, containerSelector) {
+  constructor({ items, renderer }, containerSelector) {
     this._items = items;
     this._renderer = renderer;
     this._container = document.querySelector(containerSelector);
   }
 
-    //вставляет element методом prepend в поле _container
+  //вставляет element методом prepend в поле _container
 
-  addItem (element) {
+  addItem(element) {
     this._container.prepend(element);
   }
 
 
   renderItems() {
-
+    this._items.forEach((item) => {
+      this._renderer(item);
+    });
   }
 
 
